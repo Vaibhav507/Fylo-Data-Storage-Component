f74b6391d1748de9dea80c773d58baf38dbbb210
unknown <vaibhavpratham507@gmail.com>
2024-06-04 21:52:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Card from './Card'
+
+function App() {
+
+  return (
+    <Card />
+  )
+}
+
+export default App
